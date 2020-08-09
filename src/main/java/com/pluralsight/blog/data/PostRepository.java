d2c82e4137d7d15ed1ea1c09e9eb8d5d287f2b6d
godreabhishek <68770287+godreabhishek@users.noreply.github.com>
2020-08-09 19:33:58 +0530

    Merge branch 'module1-solution' of https://github.com/pluralsight-projects/java-spring-basic-blog into pluralsight-projects-module1-solution
    # Conflicts:
    #	src/main/java/com/pluralsight/blog/BlogController.java
    #	src/main/resources/templates/home.html

--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -1,16 +1,22 @@
 package com.pluralsight.blog.data;
 
 import com.pluralsight.blog.model.Post;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 @Component
 public class PostRepository {
+    @Autowired
+    Post post;
 
     public List<Post> getAllPosts() {
-        return null;
+        return Arrays.asList(new Post[]{});
+
     }
 
 
