0733893f78ed01f0749e8c03ab6c1a861a06900c
godreabhishek <68770287+godreabhishek@users.noreply.github.com>
2020-08-09 17:55:15 +0530

    Initial commit

--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -2,14 +2,18 @@ package com.pluralsight.blog.data;
 
 import com.pluralsight.blog.model.Post;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+
 import java.util.List;
 
 @Component
 public class PostRepository {
 
+    @GetMapping("/getPosts")
     public List<Post> getAllPosts() {
         return null;
     }
+
 
     public Post findById(Long id) {
         return null;
